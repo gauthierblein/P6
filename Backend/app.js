5f9f5bf06949cbc42ffe0c48e4db98d4e899a902
bleingauthier <bleingauthier@gmail.com>
2022-11-02 10:16:17 +0100

    creation des routes et methodes user

---
+++ Backend/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require ('body-parser');
+const mongoose = require('mongoose');
+
+const userRoutes = require("./routes/user");
+
+const app = express();
+app.use(express);
+
+
+// connexion to mongoDB
+mongoose.connect('mongodb+srv://gauthierblein:Nina2016@cluster0.f0lmxbl.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+// Ajout de CORS aux headers
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.use((req, res) => {
+    res.json({ message: 'Votre requête a bien été reçue !' }); 
+ });
+
+/*app.use("/api/auth", userRoutes);*/
+
+module.exports = app;
---
+++ Backend/models/user.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+/* PlugIn pour empêcher les attaques par injection */
+const sanitizerPlugin = require('express-mongo-sanitize');
+
+const userSchema = mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: [true, "Veuillez entrer votre adresse email"],
+    match: [/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/, "Veuillez entrer une adresse email correcte"]
+    },
+  password: {
+    type: String,
+    required: [true, "Veuillez choisir un mot de passe"] 
+    }
+});
+
+userSchema.plugin(uniqueValidator);
+userSchema.plugin(sanitizerPlugin);
+
+module.exports = mongoose.model('User', userSchema);
